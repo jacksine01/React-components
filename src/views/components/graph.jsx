bc224bd795b7dba8d02b9b80ba5402333331a6db
jacksine01 <156636808+jacksine01@users.noreply.github.com>
2024-04-26 18:11:40 +0530

    feat : First commit

--- src/views/components/graph.jsx
+++ src/views/components/graph.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 
-export const Graph = () => {
-  return <div className="text-center">grap</div>;
-};
+const ENDPOINT = "http://localhost:8001";
+
+export function Graph() {
+  const [randomNumber, setRandomNumber] = useState(null);
+
+  useEffect(() => {
+    const socket = io(ENDPOINT);
+
+    socket.on("subscribeToRandomNumbers", (value) => {
+      console.log(value);
+      setRandomNumber(value);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return <div>{<p>Random Number: {randomNumber}</p>}</div>;
+}
